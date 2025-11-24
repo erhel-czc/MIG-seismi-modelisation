f6a19cfd9b0e40c93f718a709bc125cacc01e0eb
erhelito <erhelito@yahoo.fr>
2025-11-24 20:23:53 +0100

    remove unused imports

--- PR_QDYN_RNS.py
+++ PR_QDYN_RNS.py
@@ -1,9 +1,4 @@
-import os
 import numpy as np
-import matplotlib
-# # Use a non-interactive backend when no DISPLAY is available (headless environments)
-# if not os.environ.get('DISPLAY'):
-#     matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pickle
 
