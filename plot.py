a2641ec56124129c3d882e547449e086fa84c2e2
Louis Cristante <louis.cristante@etu.minesparis.psl.eu>
2025-11-26 10:27:03 +0100

    plot for leonard

--- plot.py
+++ plot.py
@@ -3,8 +3,8 @@ import matplotlib.pyplot as plt
 
 path = 'Results/PR_QDYN_RNS_P/pressure1'
 data = Result.load_results(path)
-data.slip_rate_evolution() # Plot slip rate evolution58479002277847_1.0.pkl") # Load previously saved results in the Res
-data.phase_portrait() # Plot phase portrait
+data.slip_rate_evolution(save = True, path = 'images/pressure/', name='pressure1') # Plot slip rate evolution58479002277847_1.0.pkl") # Load previously saved results in the Res
+data.phase_portrait(save = True, path = 'images/pressure/', name='pressure1') # Plot phase portrait
 if data.Tau != None:
     data.tau_evolution() # Plot shear stress evolution
 if data.Sigma_n != None:
