2901dde51d9c90ad79fa8f62ca74b2f9591f93fc
Louis Cristante <louis.cristante@etu.minesparis.psl.eu>
2025-11-26 10:14:01 +0100

    Plot adjustements

--- plot.py
+++ plot.py
@@ -5,6 +5,8 @@ path = 'Results/PR_QDYN_RNS_P/pressure1'
 data = Result.load_results(path)
 data.slip_rate_evolution() # Plot slip rate evolution58479002277847_1.0.pkl") # Load previously saved results in the Res
 data.phase_portrait() # Plot phase portrait
-#data.tau_evolution() # Plot shear stress evolution
-#data.sigma_evolution() # Plot normal stress evolution
+if data.Tau != None:
+    data.tau_evolution() # Plot shear stress evolution
+if data.Sigma_n != None:
+    data.sigma_evolution() # Plot normal stress evolution
 plt.show()
