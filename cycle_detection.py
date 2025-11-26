2289fc5f8f22339cfecf3523ce325d06e6cc0ae9
Erhel czc <erhelito@yahoo.fr>
2025-11-26 15:19:59 +0100

    use  the new fft cycle period module to determine period of the signal

--- cycle_detection.py
+++ cycle_detection.py
@@ -1,46 +1,22 @@
-# a program containing cycle detection functions
-"""
-CONSIDÉRER QU'À PARTIR DE LA MOITIÉ DU TEMPS
-"""
-
 from result import Result
 import numpy as np
 import matplotlib.pyplot as plt
 
 path = 'Results/PR_QDYN_RNS/tests.pkl'
 data = Result.load_results(path)
-# data.slip_rate_evolution()
 taux=0.5
 nb_periode_pour_securite=1
 
 signal = data.Phi
 time = data.T
 
-sampling_rate = 1 / np.mean(np.diff(time))
-fft = np.fft.rfft(signal)
-freqs = np.fft.rfftfreq(len(signal), d=1/sampling_rate)
-periode = 1 / freqs[np.argmax(np.abs(fft[1:])) + 1]
-
-"""""
-print(periode)
-
-
-plt.figure('FFT of v(t)')
-plt.plot(freqs[1:], np.abs(fft[1:]))
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Amplitude')
-plt.title('FFT of v(t)')
-plt.grid()
-
-plt.show()
-"""""
-
+period = data.fft_dominant_period()
 
 N=len(time)
 max=signal.max()
 min=signal.min()
 threshhold=taux*(max-min)+min
-duree_securite=nb_periode_pour_securite*periode
+duree_securite=nb_periode_pour_securite*period
 temps_debut=duree_securite
 temps_fin=time[-1]-duree_securite
 
