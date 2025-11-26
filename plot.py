a600cb0e95548f0d011d3a00611539c46abf3616
Erhel czc <erhelito@yahoo.fr>
2025-11-26 11:09:52 +0100

    improve plot for Mw

--- plot.py
+++ plot.py
@@ -5,10 +5,12 @@ path = 'Results/PR_QDYN_RNS_P/pressure1'
 data = Result.load_results(path)
 data.slip_rate_evolution(save = True, path = 'images/pressure/', name='pressure1') # Plot slip rate evolution58479002277847_1.0.pkl") # Load previously saved results in the Res
 data.phase_portrait(save = True, path = 'images/pressure/', name='pressure1') # Plot phase portrait
+
 if data.Tau != None:
     data.tau_evolution() # Plot shear stress evolution
 if data.Sigma_n != None:
     data.sigma_evolution() # Plot normal stress evolution
+
 plt.show()
 
 data.magnitude() # Compute moment magnitude
--- plot_Mw_lenght_fault.py
+++ plot_Mw_lenght_fault.py
@@ -1,14 +1,23 @@
 from result import Result
 import matplotlib.pyplot as plt
+import numpy as np
 
 
 def plot_Mw_vs_lenght_fault(num_save):
-    paths = [f'/Users/rico/Desktop/Mines/MIG/Semaine 2/MIG-seismi-modelisation/Results/PR_QDYN_RNS_modele_oriente/Lenght_Fault_varies/Lenght Fault {i}' for i in range(num_save)]
+    paths = [f'Results/PR_QDYN_RNS_modele_oriente/Lenght_Fault_varies/Lenght Fault {i}' for i in range(num_save)]
     data = [Result.load_results(path) for path in paths]
-    lenght_faults = [d.pd.lenght_fault for d in data]
-    Mw = [d.magnitude(print_result=False) for d in data]
+    lenght_faults = np.array([d.pd.lenght_fault for d in data])
+    Mw = np.array([d.magnitude(print_result=False) for d in data])
 
-    plt.plot(lenght_faults, Mw, marker='o')
+    plt.figure('Mw vs Lenght Fault')
+
+    plt.scatter(lenght_faults, Mw, color='black', marker='+')
+    plt.plot(lenght_faults, Mw,'-k', alpha=0.3)
+    plt.xlabel('Lenght Fault (m)')
+    plt.ylabel('Moment Magnitude (Mw)')
+    plt.title('Moment Magnitude vs Lenght Fault')
+    plt.grid()
+    
     plt.show()
 
 plot_Mw_vs_lenght_fault(100)
